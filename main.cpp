c9926cee06c654efde332e90386a61e9c6276b7b
Edia <64588911+edia8@users.noreply.github.com>
2024-12-10 20:27:38 +0200

    - fixat graficele punctate - implementat "camera mobila" - adaugat functie de nightmode

--- main.cpp
+++ main.cpp
@@ -4,25 +4,21 @@
 #include    <cmath>
 #include    <string>
 #include    <vector>
-#include    <sstream>
 #include    <map>
-#include    <functional>
 #include    <Windows.h>
-
 using namespace std;
 
 // Dimensiunile ecranului grafic
 int WIDTH = GetSystemMetrics(SM_CXSCREEN);
 int HEIGHT = GetSystemMetrics(SM_CYSCREEN);
-int SCREEN_ORIGIN_X = WIDTH / 2;
-int SCREEN_ORIGIN_Y = HEIGHT / 2;
-int ORIGIN_X = SCREEN_ORIGIN_X;
-int ORIGIN_Y = SCREEN_ORIGIN_Y;
+int ORIGIN_X = WIDTH / 2;
+int ORIGIN_Y = HEIGHT / 2;
 int CAMERA_X = 0;
 int CAMERA_Y = 0;
 bool FULSCREEN = 1;
+bool isDarkModeOn = 0;
 
-int scale = 100;
+int SCALE = 50;
 
 //computarea de functii
 
@@ -58,7 +54,6 @@ double prelucreazaFunctie(string token, double x) {
         return 0;
     }
 }
-
 vector<string> infixToPostfix(const string& expression) {
     stack<string> operators;         // Stivă pentru operatori și funcții
     vector<string> output;           // Rezultatul în notare postfixată
@@ -162,44 +157,69 @@ double evaluareExpresie(const vector<string>& postfix, double xVal) {
     return expresieConvertita.top();
 }
 
-void drawAxis() {
-    setcolor(BLACK);
-    line(ORIGIN_X, 0, ORIGIN_X, HEIGHT);
-    line(0, ORIGIN_Y, WIDTH, ORIGIN_Y);
-    for (int i = ORIGIN_X; i < WIDTH; i += scale) {
-        line(i, ORIGIN_Y - 25 * scale / 100, i, ORIGIN_Y + 25 * scale / 100);
-    }
-    for (int i = ORIGIN_X; i > 0; i -= scale) {
-        line(i, ORIGIN_Y - 25 * scale / 100, i, ORIGIN_Y + 25 * scale / 100);
-    }
-    for (int i = ORIGIN_Y; i < HEIGHT; i += scale) {
-        line(ORIGIN_X - 25 * scale / 100, i, ORIGIN_X + 25 * scale / 100, i);
-    }
-    for (int i = ORIGIN_Y; i > 0; i -= scale) {
-        line(ORIGIN_X - 25 * scale / 100, i, ORIGIN_X + 25 * scale / 100, i);
-    }
+pair<double, double> ConversieCoord(int screenX, int screenY) {
+    double cartesianX = (screenX - ORIGIN_X - CAMERA_X) / static_cast<double>(SCALE);
+    double cartesianY = -(screenY - ORIGIN_Y - CAMERA_Y) / static_cast<double>(SCALE);
+    return { cartesianX, cartesianY };
 }
 
-void drawfunction(vector<string> postfix)
-{
-    for (double X_POZ = 0;X_POZ < WIDTH;X_POZ+=1) {
-        double Y_POZ = evaluareExpresie(postfix,(X_POZ-WIDTH/2)/WIDTH*(WIDTH/scale));
-        
-        int x = (int)(X_POZ), y = (int)(-Y_POZ*scale);
+void drawAxis(int culoare) {
+    //axele de coordonate
+    setfillstyle(SOLID_FILL, culoare);
+    bar(ORIGIN_X + CAMERA_X, 0, ORIGIN_X + CAMERA_X+1, HEIGHT); //OY
+    bar(0, ORIGIN_Y + CAMERA_Y, WIDTH, ORIGIN_Y + CAMERA_Y + 1); //OX
 
-        putpixel(x,y+HEIGHT/2, RED);
+    int minX = (0 - ORIGIN_X - CAMERA_X) / SCALE; 
+    int maxX = (WIDTH - ORIGIN_X - CAMERA_X) / SCALE; 
+    int minY = (ORIGIN_Y + CAMERA_Y - HEIGHT) / SCALE; 
+    int maxY = (ORIGIN_Y + CAMERA_Y) / SCALE; 
+
+    for (int i = minX; i <= maxX;i++) {
+        int x = ORIGIN_X + CAMERA_X + i * SCALE;
+        bar(x, ORIGIN_Y + CAMERA_Y - 5, x+1, ORIGIN_Y + CAMERA_Y + 5);
+    }
+    for (int i = minY;i <= maxY; i++ ) {
+        int y = ORIGIN_Y + CAMERA_Y - i * SCALE;
+        bar(ORIGIN_X + CAMERA_X  - 5,y, ORIGIN_X + CAMERA_X + 5, y+1);
+    }
+
+}
+
+void drawfunction(vector<string> postfix) //adaugare parametru de culoare pt a desena mai multe functii simultan
+{
+    double PreviousY = evaluareExpresie(postfix, (0 - ORIGIN_X - CAMERA_X) / SCALE);
+    int prev_y = (int)(PreviousY * SCALE);
+    prev_y = -prev_y + HEIGHT / 2 + CAMERA_Y;
+    putpixel(0, prev_y+HEIGHT/2+CAMERA_Y,RED);
+
+    for (double X_POZ = 1;X_POZ < WIDTH;X_POZ+=1) {
+        double Y_POZ = evaluareExpresie(postfix, (X_POZ - ORIGIN_X - CAMERA_X)/SCALE); // modific cu coord lui x
+        
+        int x = (int)(X_POZ), y = (int)(Y_POZ*SCALE);
+        y = -y + HEIGHT / 2 + CAMERA_Y; //conversia pt a plasa punctul corect pe ecran, deoarece originea ecranului este in coltul stanga-sus
+
+        //fix pentru afisarea graficelor punctat
+
+        if (abs(y - prev_y) > 2 && abs(y-prev_y) < 10000) {   
+            setfillstyle(SOLID_FILL, RED);
+            bar(x - 1, y, x, prev_y);
+        }
+        else
+            putpixel(x,y, RED);
+        prev_y = y;
         //cout << x << " " << y << '\n';
     }
 }
-void drawfunction2(vector<string> postfix)//verificarea de valori ale functiei(pt debug)
+
+void DarkMode(int &fundal, int &axe)
 {
-    for (double X_POZ = 0;X_POZ < WIDTH;X_POZ += 1) {
-        double Y_POZ = evaluareExpresie(postfix, (X_POZ - WIDTH / 2) / WIDTH * ( WIDTH / scale));
-
-        int x = (int)(X_POZ), y = (int)(-Y_POZ * scale);
-
-        putpixel(x, y + HEIGHT / 2, RED);
-        cout << x << " " << y << '\n';
+    if (isDarkModeOn) {
+        fundal = 0;
+        axe = 15;
+    }
+    else {
+        fundal = 15;
+        axe = 0;
     }
 }
 
@@ -209,15 +229,15 @@ void fereastra()
         WIDTH = GetSystemMetrics(SM_CXSCREEN);
         HEIGHT = GetSystemMetrics(SM_CYSCREEN);
         closegraph();
-        SCREEN_ORIGIN_Y = HEIGHT / 2;
-        SCREEN_ORIGIN_X = WIDTH / 2;
+        ORIGIN_Y = HEIGHT / 2;
+        ORIGIN_X = WIDTH / 2;
         initwindow(WIDTH, HEIGHT, "", -3, -3);
         delay(100);
     }else {
         WIDTH = 1600;HEIGHT = 1200;
         closegraph();
-        SCREEN_ORIGIN_Y = HEIGHT / 2;
-        SCREEN_ORIGIN_X = WIDTH / 2;
+        ORIGIN_Y = HEIGHT / 2;
+        ORIGIN_X = WIDTH / 2;
         initwindow(WIDTH, HEIGHT, ".");
         delay(100);
     }
@@ -233,17 +253,21 @@ bool scroll(int up_or_down)
 int main() {
 int aP = 0;
     //int gd = DETECT, gm=DETECT;
-
 string expression;
 cout << "Introdu expresia matematica: ";
 getline(cin, expression);
 cout << expression;
 vector<string> postfix = infixToPostfix(expression);
 
+for (auto i : postfix)
+    cout << i << " ";
 
 
+
+    int culoareFundal, culoareAxe;
+    DarkMode(culoareFundal, culoareAxe);
     fereastra();
-    drawAxis();
+    drawAxis(culoareAxe);
    // drawfunction2(postfix);
     do {
         //trecerea ferestrei din windowed in fulscreen
@@ -254,27 +278,67 @@ vector<string> postfix = infixToPostfix(expression);
         setactivepage(aP);
 		setvisualpage(1 - aP);
         cleardevice();
-        setbkcolor(WHITE);
-        SCREEN_ORIGIN_Y = ORIGIN_Y + CAMERA_Y;
-        SCREEN_ORIGIN_X = ORIGIN_X + CAMERA_X;
-		drawAxis();
+//        if (ismouseclick(WM_RBUTTONDOWN))
+//            isDarkModeOn = !isDarkModeOn;
+        DarkMode(culoareFundal, culoareAxe);
+        setbkcolor(culoareFundal);
+		drawAxis(culoareAxe);
         drawfunction(postfix);
         if (GetKeyState(VK_OEM_PLUS) & 0x8000) {
-            if (scale >= 200 && scale<=500)
-                scale += 50;
-            else
-                scale += 10;
-        }
-        else if (GetKeyState(VK_OEM_MINUS) & 0x8000) {
-            if (scale - 10 != 0) {
-                if (scale >= 200)
-                    scale -= 50;
-                else
-                    scale -= 10;
+            if (SCALE >= 200 && SCALE <= 500) {
+                SCALE += 25;
+                delay(10);
+            }
+            else {
+                SCALE += 10;
+                delay(10);
             }
         }
+        else if (GetKeyState(VK_OEM_MINUS) & 0x8000) {
+            if (SCALE >25) {
+                if (SCALE >= 200) {
+                    SCALE -= 25;
+                }
+                else {
+                    SCALE -= 10;
+                }
+            }
+        }
+
+        if (ismouseclick(WM_LBUTTONDOWN)) {
+            int x, y;
+            getmouseclick(WM_LBUTTONDOWN, x, y);
+
+            // Transformă în coordonate carteziene
+            pair<double, double> coord = ConversieCoord(x, y);
+
+            // Marchează punctul pe ecran
+            setcolor(RED);
+            circle(x, y, 5);
+
+            // Afișează coordonatele în consolă
+            cout << "Coordonate carteziene: (" << coord.first << ", " << coord.second << ")" << endl;
+        }
+        int CameraMove = 2*SCALE;
         if (GetKeyState(VK_UP) & 0x8000)
-            CAMERA_X += 10;
+            CAMERA_Y += CameraMove;
+        if (GetKeyState(VK_DOWN) & 0x8000)
+            CAMERA_Y -= CameraMove;
+        if (GetKeyState(VK_RIGHT) & 0x8000)
+            CAMERA_X -= CameraMove;
+        if (GetKeyState(VK_LEFT) & 0x8000)
+            CAMERA_X += CameraMove;
+        if (GetKeyState(VK_CONTROL) & 0x8000) {
+            CAMERA_X = 0;
+            CAMERA_Y = 0;
+            SCALE = 50;
+        }
+        if (GetKeyState(VK_F7) & 0x8000) {
+            int x, y;
+            getmouseclick(WM_LBUTTONDOWN, x, y);
+            double X = (double)x;
+            cout << (X - WIDTH / 2) / WIDTH * (WIDTH / SCALE) << '\n';
+        }
 
     aP = 1 - aP;
     } while (!(GetKeyState(VK_ESCAPE) & 0x8000));
